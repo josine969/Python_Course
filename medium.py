9159d79855b89d424e87d40522d39ec6f29c0fc8
josine969 <jlusipa@hotmail.com>
2022-09-12 22:20:08 +0200

    new exercise, med

---
+++ medium.py
@@ -0,0 +1,10 @@
+# Create a program that retunr medium from an array
+
+# Import statistics module
+import statistics
+# Define array
+numbers = [1, 2, 3, 14, 15, 6, 7]
+# Calculate med
+med = statistics.median(numbers)
+# Print output
+print(med)
